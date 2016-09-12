963c7effaa43b47f616a22063bd01313f8232ec3
wacky6.AriesMBP <416707889@qq.com>
2016-09-12 14:29:55 +0800

    refine Login.vue
    1. implement remember me feature

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,41 +1,49 @@
 <template>
   <div :busy="busy">
     <h3>登录</h3>
-    <form novalidate v-el:form enctype="application/x-www-form-urlencoded">
 
-      <div class="field">
-        <label>
-          <span>用户</span>
-          <input name="user" type="text" :disabled="busy"></input>
-        </label>
-      </div>
-
-      <div class="field">
-        <label>
-          <span>密码</span>
-          <input name="password" type="password" :disabled="busy"></input>
-        </label>
-      </div>
-
-      <div class="field ctrl">
-        <label>
-          <span>记住我</span>
-          <input id="remember-credential" type="checkbox" v-model="remember" :disabled="busy"></input>
-        </label>
-      </div>
-
-    </form>
-
-    <div v-if="wrongCred">
-      <h4>用户名或密码不正确</h4>
+    <div v-if="tryStored">
+      <h4>正在登录</h4>
     </div>
 
-    <button
-      :disabled="!test && (busy)"
-      :busy="busy"
-      @click.prevent="login"
-    >提交
-    </button>
+    <div v-if="!tryStored">
+      <form novalidate v-el:form enctype="application/x-www-form-urlencoded">
+
+        <div class="field">
+          <label>
+            <span>用户</span>
+            <input name="user" type="text" :disabled="busy"></input>
+          </label>
+        </div>
+
+        <div class="field">
+          <label>
+            <span>密码</span>
+            <input name="password" type="password" :disabled="busy"></input>
+          </label>
+        </div>
+
+        <div class="field ctrl">
+          <label>
+            <span>记住我</span>
+            <input id="remember-credential" type="checkbox" v-model="remember" :disabled="busy"></input>
+          </label>
+        </div>
+
+      </form>
+
+      <div v-if="wrongCred">
+        <h4>用户名或密码不正确</h4>
+      </div>
+
+      <button
+        :disabled="!test && (busy)"
+        :busy="busy"
+        @click.prevent="login"
+      >提交
+      </button>
+    </div>
+
   </div>
 </template>
 
@@ -47,28 +55,82 @@
 <script>
   import Vue from 'vue'
   import getResponseMessage from '../lib/guess-response-message'
+  const TOKEN_KEY = '_token'
+
+  function getStoredToken() {
+    if (Vue.http.headers.common['Authorization'])
+      return Vue.http.headers.common['Authorization'].substr('Bearer '.length)
+    if (window.localStorage)
+      return window.localStorage.getItem(TOKEN_KEY)
+  }
+
+  function isTokenStored() {
+    // check if user want to store token
+    return window.localStorage && window.localStorage.getItem(TOKEN_KEY)
+  }
+
+  function setToken(token, remember = false) {
+    if (!token)
+      return clearToken()
+    Vue.http.headers.common['Authorization'] = 'Bearer '+token
+    if (remember && window.localStorage)
+      window.localStorage.setItem(TOKEN_KEY, token)
+  }
+
+  function clearToken() {
+    delete Vue.http.headers.common['Authorization']
+    if (window.localStorage)
+      window.localStorage.removeItem(TOKEN_KEY)
+  }
+
   export default {
     data() {
       return {
         busy: false,
+        tryStored: false,
         remember: false,
         wrongCred: false
       }
     },
+    ready() {
+      // detect stored token
+      let token = getStoredToken()
+      if (token) {
+        // try to refresh token
+        this.remember = isTokenStored()
+        this.tryStored = true
+        this.busy = true
+        setToken(token)
+        this.$http.get('login')
+        .then( (res) => {
+          this.busy = false
+          this.loginSuccess( res.json().token )
+          this.remember = false
+          this.tryStored = false
+        })
+        .catch( (res) => {
+          this.busy = false
+          this.remember = false
+          this.tryStored = false
+          clearToken()
+          let msg = getResponseMessage(res)
+          // TODO: complain about error
+        })
+      }
+    },
     methods: {
+      loginSuccess(token) {
+        //this.$els.form.reset()
+        this.$router.replace(this.$route.query.next || '/')
+        setToken(token, this.remember)
+      },
       login() {
         this.busy = true
         this.$http.post('login', new FormData(this.$els.form))
         .then( (res) => {
           this.busy = false
           this.wrongCred = false
-          //this.$els.form.reset()
-          this.$router.replace(this.$route.query.next || '/')
-          // remember token
-          let token = res.json().token
-          Vue.http.headers.common['Authorization'] = 'Bearer '+token
-          if (this.remember)
-            window.localStorage.setItem('_token', res.json().token)
+          this.loginSuccess( res.json().token )
         })
         .catch( (res) => {
           this.busy = false
--- src/lib/guess-response-message.js
+++ src/lib/guess-response-message.js
@@ -1,9 +1,13 @@
 'use strict'
 
 export default function GuessResponseMessage(res) {
-  try {
-    return res.json().message
-  }catch(e) {
-    return res.text()
+  if (res instanceof Error) {
+    return `Error: ${res.message}`
+  }else{
+    try {
+      return res.json().message
+    }catch(e) {
+      return res.text()
+    }
   }
 }
