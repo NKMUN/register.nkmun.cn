fad717f9ff2530986f5e5bc2f83d7556c0f21f3b
wacky6.AriesMBP <416707889@qq.com>
2016-10-14 15:38:07 +0800

    refine token storage behavior

--- src/lib/token-store.js
+++ src/lib/token-store.js
@@ -6,6 +6,8 @@ const TOKEN_KEY = '_token'
 export function getToken() {
   if (Vue.http.headers.common['Authorization'])
     return Vue.http.headers.common['Authorization'].substr('Bearer '.length)
+  if (window.sessionStorage)
+    return window.sessionStorage.getItem(TOKEN_KEY)
   if (window.localStorage)
     return window.localStorage.getItem(TOKEN_KEY)
 }
@@ -19,12 +21,16 @@ export function setToken(token, remember = false) {
   if (!token)
     return clearToken()
   Vue.http.headers.common['Authorization'] = 'Bearer '+token
+  if (window.sessionStorage)
+    window.sessionStorage.setItem(TOKEN_KEY, token)
   if (remember && window.localStorage)
     window.localStorage.setItem(TOKEN_KEY, token)
 }
 
 export function clearToken() {
   delete Vue.http.headers.common['Authorization']
+  if (window.sessionStorage)
+    window.sessionStorage.removeItem(TOKEN_KEY)
   if (window.localStorage)
     window.localStorage.removeItem(TOKEN_KEY)
 }
