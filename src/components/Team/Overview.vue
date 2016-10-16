4ad3a0759a0b99342a0433324ef37faa7eada141
wacky6.AriesMBP <416707889@qq.com>
2016-10-16 13:41:03 +0800

    add Team/Overview quote display

--- src/components/Team/Overview.vue
+++ src/components/Team/Overview.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="team-overview">
 
+    <div class="section own-quote">
+      <h4>已分配名额</h4>
+      <ul>
+        <li v-for="$ in committees | filterBy hasQuote">
+          <span class="name">{{ committee_name[$.dbId] }}</span>
+          <span class="quote">{{ data.committee[$.dbId] }}</span>
+          <button @click="giveup.committee = $.dbId">放弃...</button>
+        </li>
+      </ul>
+    </div>
+
     <div class="section school">
       <h4>学校信息</h4>
       <div class="field">
@@ -30,12 +41,14 @@
 
 <script>
   import getResponseMessage from '../../lib/guess-response-message'
-  import {groups as COMMITTEE_GROUPS} from '../../def/committee'
+  import {groups as COMMITTEE_GROUPS, ungrouped as committees, idMapping as committee_name} from '../../def/committee'
 
   export default {
     props: ['data'],
     created() {
       this.groups = COMMITTEE_GROUPS
+      this.committees = committees
+      this.committee_name = committee_name
     },
     computed: {
       state() { return this.data && this.data.state ? this.data.state : 'registered' },
@@ -43,6 +56,11 @@
       todo_hotel()    { return this.state==='registered' || this.state==='quote_confirmed' },
       todo_payment()  { return this.state==='registered' || this.state==='quote_confirmed' || this.state==='hotel_confirmed' },
       todo_attend()   { return this.state==='finished' }
+    },
+    methods: {
+      hasQuote({dbId}) {
+        return this.data.committee && this.data.committee[dbId] > 0
+      }
     }
   }
 </script>
