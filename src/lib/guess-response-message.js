abbc9e38771d198aea566ea7447ff77ad26238d3
wacky6.AriesMBP <416707889@qq.com>
2016-11-03 23:47:21 +0800

    update guess-response-message for vue-resource 1.0.3

--- src/lib/guess-response-message.js
+++ src/lib/guess-response-message.js
@@ -4,10 +4,7 @@ export default function GuessResponseMessage(res) {
   if (res instanceof Error) {
     return `Error: ${res.message}`
   }else{
-    try {
-      return res.json().message
-    }catch(e) {
-      return res.text()
-    }
+      return res.json().then( ({message}) => message )
+             .catch( e => res.text() )
   }
 }
